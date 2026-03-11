6caea06469e6f9b99e9900a5002ef0b2e2c1014e
L0rdxD <159791952+L0rdxD@users.noreply.github.com>
2026-03-11 03:50:31 +0300

    Реализуем список аниме по жанрам и выведем все что есть в дата в правильном порядке и изменение пути картинок на локальные

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -2,9 +2,24 @@
 const bgElements = () => {
     const elements = document.querySelectorAll('.set-bg')
 
-    elements.forEach((elem) => {
-        //отвечает за подключеине пути картинки
-        elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+    elements.forEach((elem, index) => {
+        const src = elem.dataset.setbg
+        if (!src) return
+
+        const img = new Image()
+
+        img.onload = () => {
+            elem.style.backgroundImage = `url(${src})`
+        }
+
+        img.onerror = () => {
+            // запасная локальная картинка, если внешний хост блокирует
+            const fallbackIndex = (index % 6) + 1
+            const fallback = `img/trending/trend-${fallbackIndex}.jpg`
+            elem.style.backgroundImage = `url(${fallback})`
+        }
+
+        img.src = src
     })
 }
 
